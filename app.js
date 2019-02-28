617846def4ca99dc1db2b10e0026583c7da14a8a
Alan Munoz Lopez <alanmunozlopez@Alans-MacBook-Pro.local>
2019-02-27 21:10:33 -0300

    AM - nodemon and bodyparser added

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // Set express app
 const app = express();
+
+// Parse requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // hello dude
 app.get('/api/v1/hello', (req, res) => {
   res.status(200).send({
     success: true,
     message: 'The hello is here dude',
-    // hello: { msg: 'Hey Alan!' }
+    hello: { msg: 'Hey Alan! You are welcome!' }
   })
 });
 
